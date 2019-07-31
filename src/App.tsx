886e700b715b471aaab3e5d9b0db017d4c4daf56
sirawat <sirawat_gun165@hotmail.com>
2019-08-01 02:03:26 +0700

    context ex

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import { UserContext } from "./context/UserContext";
+import Home from './containers/home/Home';
 
-const App: React.FC = () => {
+const AppRouter: React.FC = () => {
+  const [user, setUser] = useState(null);
+  const value = useMemo(() => ({ user, setUser }), [user, setUser]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,9 +24,28 @@ const App: React.FC = () => {
         >
           Learn React
         </a>
+
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about/">About</Link>
+                </li>
+              </ul>
+            </nav>
+            <UserContext.Provider value={value}>
+              <Route path="/" exact component={Home} />
+              {/* <Route path="/about/" component={About} /> */}
+            </UserContext.Provider>
+          </div>
+        </Router>
       </header>
     </div>
   );
 }
 
-export default App;
+export default AppRouter;
---
+++ src/context/UserContext.tsx
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+// interface User {
+//   user:
+//   setUser
+// }
+export const UserContext = createContext<any | null>(null);
